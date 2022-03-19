1b1a8f412533dbdf60f483bcdd9bdf09c8f40358
momo0170 <momo0170@naver.com>
2022-03-19 23:42:08 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ document.addEventListener('scroll', () => {
     
   }
 });
+
+// 메뉴를 클릭했을 때 해당 메뉴로 스크롤
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == undefined) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+});
