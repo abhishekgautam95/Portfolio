06cf237288a45c759bedd4e46a310ec9ca36a190
momo0170 <momo0170@naver.com>
2022-03-20 11:25:43 +0900

    JS: Home Contact

--- main.js
+++ main.js
@@ -11,19 +11,29 @@ document.addEventListener('scroll', () => {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
-    
   }
 });
 
 // 메뉴를 클릭했을 때 해당 메뉴로 스크롤
-const navbarMenu = document.querySelector('.navbar__menu');
+// event.target -> 현재 이벤트가 발생한 요소를 반환
+const navbarMenu = document.querySelector('.navbar__menu'); // div.navbar__menu
 navbarMenu.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
+  const target = event.target; //li 중 이벤트가 발생한 li 요소를 target 변수에 저장
+  const link = target.dataset.link; // li 요소의 link 속성을 link 변수에 저장
   if(link == undefined) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+  scrollIntoView(link);
 });
+
+// Contact me 버튼을 눌렀을 때 해당 메뉴로 스크롤
+const contactMenu = document.querySelector('.home__container .home__contact'); 
+console.log(contactMenu);
+contactMenu.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
